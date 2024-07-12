460ae5e296077a539bec077dcd881299a8050d13
burcutosun <124238456+burcutosun@users.noreply.github.com>
2024-07-12 18:23:10 +0300

    Project completed

---
+++ src/main/java/com/workintech/s18d4/entity/Customer.java
@@ -0,0 +1,41 @@
+package com.workintech.s18d4.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="customer",schema="fsweb")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="salary")
+    private Double salary;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Account> accounts=new ArrayList<>();
+
+    @JsonManagedReference
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="address_id")
+    private Address address;
+
+}
